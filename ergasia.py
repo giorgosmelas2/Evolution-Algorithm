a25e71c8e76dd250bd54e410820dbff728011d58
giorgosmelas2 <giorgosmelas2002@gmail.com>
2024-12-03 22:42:45 +0200

    second commit

--- ergasia.py
+++ ergasia.py
@@ -5,46 +5,51 @@ distance = int(input("How many kilometers the distance is: "))
 while(distance < 0 or distance > 5000):
     distance = int(input("Distance has to be from 1 up to 5000 kilometers. Give again: "))
 
-length_and_rods = []
-length = -1
-rods = -1
+length_array = []
+rods_array = []
 i = 1
 
-while(length != 0 and rods != 0):
-
+while(True): 
     length = float(input(f"How many kilometers are the cable number {i}: "))
+
     while(length < 0 or length > 200):
         length = float(input("Length has to be from 1 up to 200 kilometers. Give again: "))
     
     rods = float(input(f"How many {length} kilometres rods there are: "))
     while(rods < 0 or rods > 100):
-        rods = float(input("The number of rods has to be from 1 up to 100. Give again: "))
+        rods = int(input("The number of rods has to be from 1 up to 100. Give again: "))
 
-    i = i + 1
-    length_and_rods.append([length, rods])
-
-
+    i += 1
+    if length != 0 and rods != 0:
+        length_array.append(length)
+        rods_array.append(rods)
+    else:
+        break
 
 def initialize_population():
     population = []
-    for i in range(5):
+
+    for _ in range(6):
         individual = []
-        for _ in range(len(length_and_rods)-1):
-            gene = np.random.randint(0, length_and_rods[_][1])
+
+        for i in range(len(length_array)):
+            gene = np.random.randint(0, rods_array[i])
             individual.append(gene)
+
         population.append(individual)
+
     return np.array(population)
 
 
-def fitness(solution):
-    total_length = sum(solution[i]*length_and_rods[_][1] for _ in range(len(length_and_rods)-1))
+def fitness_func(ga_instance, solution, solution_idx):
+    total_length = sum(solution[i]*length_array[i] for i in range(len(length_array)))
     total_connections = sum(solution) - 1 
     penalty = 0
 
     if total_length != distance:
 
         difference = total_length - distance
-        difference_percentage = ((difference) / distance) * 100 
+        difference_percentage = (difference / distance) * 100 
         
         if difference_percentage < -20:
             penalty = difference * 1000
@@ -64,14 +69,60 @@ def fitness(solution):
     total_penalty = penalty + total_connections * 10
     return total_penalty
 
+def crossover_func(parents, offspring_size, ga_instance):
+    offspring = []
+    idx = 0
+    while len(offspring) != offspring_size[0]:
+        parent1 = parents[idx % parents.shape[0], :].copy()
+        parent2 = parents[(idx + 1) % parents.shape[0], :].copy()
 
+        random_split_point = np.random.choice(range(offspring_size[1]))
 
+        parent1[random_split_point:] = parent2[random_split_point:]
 
+        offspring.append(parent1)
 
+        idx += 1
+
+    return np.array(offspring)
+            
+def mutation_func(offspring, ga_instance):
+    upper_limit_of_geme = ga_instance.gene_space
+
+    for chromosome_idx in range(offspring.shape[0]):
+        random_gene_idx = np.random.choice(range(offspring.shape[1]))
+
+        offspring[chromosome_idx, random_gene_idx] += np.random.randint(0, upper_limit_of_geme[random_gene_idx])
+
+    return offspring
+
+print(f"length_array: {length_array}")
+print(f"rods_array: {rods_array}")
+print(f"Population:\n{initialize_population()}")
+
+num_genes = len(length_array)
+initial_population = initialize_population()
+gene_space = []
+for i in range(len(rods_array)):
+    gene_space.append(rods_array[i])
+
+ga_instance = pg.GA(num_generations=50,
+                    sol_per_pop=6,
+                    num_parents_mating=2,
+                    num_genes= num_genes,
+                    initial_population=initial_population,
+                    fitness_func=fitness_func,
+                    crossover_type=crossover_func,
+                    mutation_type=mutation_func,
+                    gene_space=gene_space
+)
+
+ga_instance.run()
+
+print("Best solution:", ga_instance.best_solution())
     
 
 
-#print(f"Array length_and_rods: {length_and_rods}")
-print(f"Population: {initialize_population(length_and_rods)}")
+
 
 
